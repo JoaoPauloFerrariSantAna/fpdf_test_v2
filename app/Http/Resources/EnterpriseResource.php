b4fe7f07d0b129da5fc6408a802a2e3a826b928a
JoaoPauloFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-05-04 15:41:35 -0300

    feat: added two new resources

---
+++ app/Http/Resources/EnterpriseResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EnterpriseResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return array(
+			"name" => $this->name,
+			"worker_amount" => $this->worker_amount
+		);
+    }
+}

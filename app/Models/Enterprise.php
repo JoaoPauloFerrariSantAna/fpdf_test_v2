24f0100f875d087d90ba319a63f5e05ea0e3fdb0
JoaoPaulOFerrariSantAna <joaopauloferrarisantana@gmail.com>
2026-05-29 20:12:39 -0300

    feat: added method to encapsulate query in database

--- app/Models/Enterprise.php
+++ app/Models/Enterprise.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
 
 class Enterprise extends Model
 {
@@ -18,5 +20,27 @@ class Enterprise extends Model
 	public function worker(): HasMany
 	{
 		return $this->hasMany(Worker::class);
+	}
+
+	public function getSales(int $enterpriseId): Collection
+	{
+		return DB::table("enterprise_tbl")
+        ->select(
+            "enterprise_tbl.name AS ENTERPRISE_NAME",
+            "workers_tbl.name AS WHO_SOLD",
+            "product_tbl.name AS WHAT_WAS_SOLD",
+            "product_tbl.stock AS STOCK_LEFT",
+            DB::RAW("sale_tbl.amount_sold * product_tbl.price AS TOTAL_AMOUNT_SOLD"),
+            DB::RAW("SUM(sale_tbl.amount_sold) AS TOTAL_SOLD"),
+            "sale_tbl.created_at AS WHEN_SOLD"
+        )
+        ->join("workers_tbl", "workers_tbl.enterprise_id", '=', "enterprise_tbl.id")
+        ->join("worker_sale", "worker_sale.worker_id", '=', "workers_tbl.id")
+        ->join("sale_tbl", "sale_tbl.id", '=', "worker_sale.sale_id")
+        ->join("product_sales", "product_sales.sale_id", '=', "sale_tbl.id")
+        ->join("product_tbl", "product_tbl.id", '=', "product_sales.sale_id")
+        ->where("enterprise_tbl.id", '=', $enterpriseId)
+        ->groupBy("workers_tbl.name")
+        ->get();
 	}
 }
